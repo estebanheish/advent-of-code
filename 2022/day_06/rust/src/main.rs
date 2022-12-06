fd45e83f9bdbe8c6b3580cfaec38f9d898789a62
estebanheish <estebanheish@gmail.com>
2022-12-06 12:53:27 +0100

    day 6 cleanup

--- 2022/day_06/rust/src/main.rs
+++ 2022/day_06/rust/src/main.rs
@@ -1,22 +1,14 @@
+fn find(input: &str, n: usize) -> usize {
+    input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(n)
+        .position(|c| !(0..n).any(|i| (0..n).any(|y| c[i] == c[y] && y != i)))
+        .unwrap()
+        + n
+}
+
 fn main() {
     let input = include_str!("../../input.txt");
-
-    let first = input
-        .chars()
-        .collect::<Vec<char>>()
-        .windows(4)
-        .position(|c| !(0..4).any(|i| (0..4).any(|y| c[i] == c[y] && y != i)))
-        .unwrap()
-        + 4;
-
-    let second = input
-        .chars()
-        .collect::<Vec<char>>()
-        .windows(14)
-        .position(|c| !(0..14).any(|i| (0..14).any(|y| c[i] == c[y] && y != i)))
-        .unwrap()
-        + 14;
-
-    println!("{:?}", first);
-    println!("{:?}", second);
+    println!("{}\n{}", find(input, 4), find(input, 14));
 }
