fe2b15cae88921ee3cf5c93b546dea7ad5c9bb2c
estebanheish <estebanheish@gmail.com>
2022-12-19 23:52:32 +0100

    day 19 fix

--- 2022/day_19/rust/src/main.rs
+++ 2022/day_19/rust/src/main.rs
@@ -68,14 +68,16 @@ fn main() {
         .collect();
 
     let mut part1 = 0;
-    let mut part2 = 0;
     for (i, bp) in blueprints.iter().enumerate() {
         part1 += (i + 1) * geodes(*bp, 24);
-        part2 += (i + 1) * geodes(*bp, 32);
     }
+    println!("part 1: {}", part1);
 
-    println!("{}", part1);
-    println!("{}", part2);
+    let mut part2: Vec<usize> = Vec::new();
+    for bp in blueprints.iter().take(3) {
+        part2.push(geodes(*bp, 32));
+    }
+    println!("part 2: {}", part2.iter().product::<usize>());
 }
 
 fn geodes(bp: Blueprint, time: usize) -> usize {
@@ -87,8 +89,8 @@ fn geodes(bp: Blueprint, time: usize) -> usize {
         let (ore, clay, obsidian, geode) = state.resources;
         let (ore_robots, clay_robots, obsidian_robots, geode_robots) = state.robots;
 
-        if state.time_left == 0 {
-            geodes = geodes.max(geode);
+        if state.time_left == 1 {
+            geodes = geodes.max(geode + geode_robots);
             continue;
         }
 
@@ -101,7 +103,6 @@ fn geodes(bp: Blueprint, time: usize) -> usize {
             continue;
         }
         cache.insert(state);
-
         state.time_left -= 1;
 
         let new_resources = (
@@ -111,22 +112,27 @@ fn geodes(bp: Blueprint, time: usize) -> usize {
             geode + geode_robots,
         );
 
+        let mut built_ore = false;
         if ore >= bp.ore && ore_robots <= bp.max_ore {
             let mut state = state;
             state.robots.0 += 1;
             state.resources = new_resources;
             state.resources.0 -= bp.ore;
             queue.push(state);
+            built_ore = true;
         }
 
+        let mut built_clay = false;
         if ore >= bp.clay && clay_robots <= bp.obsidian.1 {
             let mut state = state;
             state.robots.1 += 1;
             state.resources = new_resources;
             state.resources.0 -= bp.clay;
             queue.push(state);
+            built_clay = true;
         }
 
+        let mut built_obsidian = false;
         if ore >= bp.obsidian.0 && clay >= bp.obsidian.1 && obsidian_robots <= bp.geode.1 {
             let mut state = state;
             state.robots.2 += 1;
@@ -134,8 +140,10 @@ fn geodes(bp: Blueprint, time: usize) -> usize {
             state.resources.0 -= bp.obsidian.0;
             state.resources.1 -= bp.obsidian.1;
             queue.push(state);
+            built_obsidian = true;
         }
 
+        let mut built_geode = false;
         if ore >= bp.geode.0 && obsidian >= bp.geode.1 {
             let mut state = state;
             state.robots.3 += 1;
@@ -143,10 +151,13 @@ fn geodes(bp: Blueprint, time: usize) -> usize {
             state.resources.0 -= bp.geode.0;
             state.resources.2 -= bp.geode.1;
             queue.push(state);
+            built_geode = true;
         }
 
-        state.resources = new_resources;
-        queue.push(state);
+        if ![built_ore, built_clay, built_obsidian, built_geode].iter().all(|t| *t == true) {
+            state.resources = new_resources;
+            queue.push(state);
+        }
     }
     geodes
 }
